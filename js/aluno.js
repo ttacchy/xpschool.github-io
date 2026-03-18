974309850dda50c135130f55e719261af56f19c3
tacchy <ttacchy@gmail.com>
2026-03-18 20:57:38 -0300

    Replace logout function with carregarAluno function

--- js/aluno.js
+++ js/aluno.js
@@ -1,4 +1,20 @@
-function logout(){
-  localStorage.removeItem("user")
-  window.location = "index.html"
+async function carregarAluno() {
+  const user = JSON.parse(localStorage.getItem("user"));
+  const db = await carregarDB();
+
+  const aluno = db.alunos.find(a => a.id === user.id);
+
+  document.getElementById("xp").innerText = aluno.xp;
+  document.getElementById("nivel").innerText = aluno.nivel;
+
+  const lista = document.getElementById("emblemas");
+  lista.innerHTML = "";
+
+  aluno.emblemas.forEach(e => {
+    const li = document.createElement("li");
+    li.innerText = e;
+    lista.appendChild(li);
+  });
 }
+
+carregarAluno();
